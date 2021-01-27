5d226ca30e67716abbd972e256ab4387032d4b6d
Maks <maksim.t00@gmail.com>
2021-01-28 00:22:23 +0300

    Подготовка для heroku

--- src/main/java/com/lbet/models/repos/UserInfoRepo.java
+++ src/main/java/com/lbet/models/repos/UserInfoRepo.java
@@ -4,11 +4,15 @@ import com.lbet.models.domain.UserInfo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public interface UserInfoRepo extends CrudRepository<UserInfo, Long> {
+    @Transactional
     UserInfo findFirstByUserid(long user_id);
+    @Transactional
     List<UserInfo> findAllByEmailUser(String loginUser);
+    @Transactional
     List<UserInfo> findFirstByEmailUserAndPasswordUser(String email, String password);
 }
